a0b67d6b4fc7b09f775cc9e34149413b6d597d53
IgorSantos1910 <igorsp06112004@gmail.com>
2026-05-17 12:24:51 -0300

    Banco de dados com LGPD e dependencias do projeto

--- database.sql
+++ database.sql
@@ -1,20 +1,24 @@
--- Tabela de usuários
 CREATE TABLE usuarios (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha BYTEA NOT NULL,
-    segredo_2fa VARCHAR(32),
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    reset_token TEXT,
-    token_expiracao TIMESTAMP
+    id               SERIAL PRIMARY KEY,
+    nome             VARCHAR(100)  NOT NULL,
+    email            VARCHAR(100)  UNIQUE NOT NULL
+                       CHECK (email = lower(email)),
+    senha            TEXT          NOT NULL,
+    segredo_2fa      VARCHAR(32),
+    criado_em        TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
+
+    reset_token      TEXT,
+    token_expiracao  TIMESTAMP,
+
+    lgpd_aceite      BOOLEAN       NOT NULL DEFAULT FALSE,
+    lgpd_versao      VARCHAR(20),
+    lgpd_data_aceite TIMESTAMPTZ
 );
 
--- Tabela de logs de acesso
 CREATE TABLE logs_acesso (
-    id SERIAL PRIMARY KEY,
-    usuario_id INT,
-    evento TEXT,
-    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id          SERIAL PRIMARY KEY,
+    usuario_id  INT,
+    evento      TEXT,
+    data_hora   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
